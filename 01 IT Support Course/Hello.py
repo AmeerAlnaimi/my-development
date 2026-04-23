d84f6dc33f8ef5fafded918659c6726e1ae6caf3
AmeerAlnaimi <ameeralnaimi@outlook.com>
2026-04-23 23:31:29 +0400

    Fresh start

---
+++ 01 IT Support Course/Hello.py
@@ -0,0 +1,9 @@
+print("Hello from my new dev enfvironment!")
+password = input("Enter Your Password Here")
+if password == "ameer12345":
+    print("Successful")
+else:
+    print("Incorrect Password")
+print("This is the end of the program")
+# * Attempt to download GitHub Desktop and install it on your computer. Then, create a new repository and push this code to the repository.
+# * Try saving it in the source control folder of your IDE and see if you can commit and push it to GitHub. (Download Git)
